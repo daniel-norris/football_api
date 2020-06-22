9f31e69dd7824459a20d1a4f94d5ac41e7b7bb14
Daniel Norris <dan.norris@hotmail.com>
2020-06-23 00:27:08 +0100

    updated unit tests to test database

--- tests/Unit/PlayerTest.php
+++ tests/Unit/PlayerTest.php
@@ -3,11 +3,18 @@
 namespace Tests\Unit;
 
 use App\Player;
+use App\Team;
 
-use PHPUnit\Framework\TestCase;
+// replaces the default PHPUnit tester
+use Tests\TestCase;
+// clears and migrates the database for each test to make database predictable
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PlayerTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     private $player;
 
     public function setUp() : void
@@ -29,5 +36,26 @@ class PlayerTest extends TestCase
     public function testFullName()
     {
         $this->assertSame("Bob Smith", $this->player->fullName());
+    }
+
+    public function testDatabase()
+    {
+        Team::create([
+            "name" => "Team 1"
+        ]);
+
+        Player::create([
+            "first_name" => "Bob",
+            "last_name" => "Smith",
+            "age" => 23,
+            "height" => 185,
+            "position" => "Forward",
+            "team_id" => 1
+        ]);
+
+        $playerFromDB = Player::all()->first();
+
+        $this->assertSame("Bob", $playerFromDB->first_name);
+
     }
 }
