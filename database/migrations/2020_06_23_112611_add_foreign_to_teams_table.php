a75cf096d1bd593196c3807525f3627224136753
Daniel Norris <dan.norris@hotmail.com>
2020-06-23 12:29:44 +0100

    created a game model

---
+++ database/migrations/2020_06_23_112611_add_foreign_to_teams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignToTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            // creating a one-to-many relationship with the games db table
+            $table->foreignId("game_id")->unsigned();
+            $table->foreign("game_id")->references("id")->on("games")->onDelete("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropColumn("game_id");
+        });
+    }
+}
